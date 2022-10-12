43f2f1ee2f78aa31db4405efa4e3fead9c32eaf5
Dorialean <lana.lakutina@mail.ru>
2022-10-12 08:23:11 +0300

    Add project files.

---
+++ StringWorker.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Concurrent;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ParrallelProj
+{
+    public sealed class StringWorker
+    {
+        public Dictionary<char, int> CountUniqueSymbols(string s) 
+        {
+            var dict = new Dictionary<char, int>();
+            foreach (char ch in s)
+            {
+                if (!dict.ContainsKey(ch))
+                {
+                    dict[ch] = 1;
+                }
+                else
+                    dict[ch]++;
+            }
+            return dict;
+        }
+
+        public async Task<ConcurrentDictionary<char, int>> CountUniqueSymbolsTask(string s)
+        {
+            var dict = new ConcurrentDictionary<char, int>();
+            foreach (char c in s.Distinct())
+            {
+                dict[c] = await Task.Run(() => CountSymbolInSequence(c, s));
+            }
+            return dict;
+            
+        }
+
+        public ConcurrentDictionary<char, int> CountUniqueSymbolsParrallel(string s)
+        {
+            var dict = new ConcurrentDictionary<char, int>();
+            Parallel.ForEach(s, ch =>
+            {
+                if (!dict.ContainsKey(ch))
+                {
+                    dict[ch] = 1;
+                }
+                else
+                    dict[ch]++;
+            });
+            return dict;
+        }
+
+        private int CountSymbolInSequence(char c, string s)
+        {
+            int count = 0;
+            foreach (char ch in s)
+            {
+                if (c == ch)
+                    count++;
+            }
+            return count;
+        }
+    }
+}
---
+++ UniqueStringBenchmark.cs
@@ -0,0 +1,45 @@
+﻿using BenchmarkDotNet.Attributes;
+using BenchmarkDotNet.Order;
+using System;
+using System.Collections.Concurrent;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ParrallelProj
+{
+    [MemoryDiagnoser]
+    [RankColumn]
+    public class UniqueStringBenchmark
+    {
+        [Params(10, 100, 1000, 100_000, 1_000_000)]
+        public int Amount { get; set; }
+
+        private string _s = string.Empty;
+        private StringWorker _sw;
+
+        [GlobalSetup]
+        public void Setup()
+        {
+            Random rnd = new();
+            for (int i = 0; i < Amount; i++)
+            {
+                _s += (char)rnd.Next(0, 256);
+            }
+            _sw = new();
+        }
+
+        [Benchmark]
+        public Dictionary<char, int> Usual_Way() => _sw.CountUniqueSymbols(_s);
+
+        [Benchmark]
+        public Task<ConcurrentDictionary<char, int>> Task_Way() => _sw.CountUniqueSymbolsTask(_s);
+
+        [Benchmark]
+        public ConcurrentDictionary<char, int> Parrallel_Way() => _sw.CountUniqueSymbolsParrallel(_s);
+
+
+
+    }
+}
